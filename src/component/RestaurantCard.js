b31f98acb3eb493fd9c416bbbbbf3fb50e0cec23
abhishekaushik1 <129890283+abhishekaushik1@users.noreply.github.com>
2024-01-06 14:12:29 +0530

    Episode - 05

---
+++ src/component/RestaurantCard.js
@@ -0,0 +1,29 @@
+import { CDN_URL } from "../utils/constants";
+const RestaurantCard = (props) => {
+    const { resData } = props;
+    const {
+      name,
+      cuisines,
+      costForTwo,
+      avgRating,
+      deliveryTime,
+      cloudinaryImageId,
+    } = resData?.data;
+    return (
+      <div className="res-card" style={{ backgroundColor: "#f0f0f0" }}>
+        <img
+          className="res-logo"
+          src={
+            CDN_URL +
+            cloudinaryImageId
+          }
+        />
+        <h3>{name}</h3>
+        <h4>{cuisines.join(", ")}</h4>
+        <h4>{avgRating} stars</h4>
+        <h4>₹{costForTwo / 100} </h4>
+        <h4>{deliveryTime} minutes</h4>
+      </div>
+    );
+  };
+  export default RestaurantCard;
